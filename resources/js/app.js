88b75d04e66da65b2937d1ad58f56403972b214a
Christopher Pitt <cgpitt@gmail.com>
2021-01-06 10:15:56 +0200

    Import galaxy components

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { Button } from '@indiefin/galaxy-button'
 
-const App = () => <div>hello world</div>
+const App = () => (
+    <div>
+        <Button handleClick={() => console.log('hello world')}>hello world</Button>
+    </div>
+)
 
 render(<App />, document.querySelector('.app'))
